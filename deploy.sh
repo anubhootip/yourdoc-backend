d073cbd711572e3dd0f3b555ffaabbc51d2a055a
Anubhooti Pathak <an499564@dal.ca>
2023-04-09 20:39:02 -0400

    ci build + test

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,13 @@ source .env
 
 # Building React output
 if [ "$NETLIFY_AUTH_TOKEN" != "" ]; then
-  rm -rf ./frontend_build.zip
-  npm install
-  cd yourdoc_frontend
-  rm -rf build
-  npm install
-  ENV=production && npm run build
-  cd ..
+  # rm -rf ./frontend_build.zip
+  # npm install
+  # cd yourdoc_frontend
+  # rm -rf build
+  # npm install
+  # ENV=production && npm run build
+  # cd ..
   node build.js NETLIFY_AUTH_TOKEN=$NETLIFY_AUTH_TOKEN NETLIFY_API_URL=$NETLIFY_API_URL
 else
   echo "No env variables found!!"
